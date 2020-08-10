d59355cbbf5558d7a8e2c7a2921d276cace7a0c9
Michel-Alexandre Broekaert <contact@broekaert.space>
2020-08-10 16:34:01 -0400

    W6D1 chal2 completed without CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get "/ask", to: "questions#ask"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/answer", to: "questions#answer"
 end
+
+# as: :ask
+# as: :answer
